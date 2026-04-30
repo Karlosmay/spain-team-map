8657584b5fb6652be5a3012e7212b614ab25e246
Karlosmay <karlos.baneira@gmail.com>
2026-04-30 16:43:44 +0200

    Create markerIcons.ts

---
+++ src/markerIcons.ts
@@ -0,0 +1,23 @@
+import L from "leaflet";
+
+const baseIcon = {
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png"
+};
+
+export const markerIcons = {
+  MPS: L.icon({
+    ...baseIcon,
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png"
+  }),
+  Manager: L.icon({
+    ...baseIcon,
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png"
+  }),
+  default: L.icon({
+    ...baseIcon,
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png"
+  })
+};
